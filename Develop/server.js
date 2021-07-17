9c6e574785479bd06a80ddcc20433c654232ceaf
scottydphillips <scotty.d.phillips@gmail.com>
2021-07-17 16:33:39 -0400

    heroku push

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req,res) => {
+	res.json(path.join(__dirname, 'public.index.html'));
+});
+
 app.get('/notes', (req,res) => {
 	res.sendFile(path.join(__dirname, './public/notes.html'));
 })
