5e06181747259a307701f81ff8582ab27d5353fa
scottydphillips <scotty.d.phillips@gmail.com>
2021-07-17 16:46:27 -0400

    heroku push

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
-const database = require('./Develop/db/db.json')
+const database = require('./db/db.json')
 
 const app = express();
 const PORT = process.env.PORT || 3000
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-	res.json(path.join(__dirname, './Develop/public/index.html'));
+	res.json(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req,res) => {
-	res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+	res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.get('/api/notes', (req,res) => {
